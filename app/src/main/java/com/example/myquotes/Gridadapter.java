e641fd6a9c05e2680be4180fabf9f16f1c064819
ashwiniswag <ashwinipal360@gmail.com>
2020-05-29 21:59:18 +0530

    My-Quotes

--- app/src/main/java/com/example/myquotes/Gridadapter.java
+++ app/src/main/java/com/example/myquotes/Gridadapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,10 +14,12 @@ public class Gridadapter extends BaseAdapter {
 
     Context applicationcontext;
     List<String> names;
+    List<Integer> images;
     LayoutInflater inflater;
 
-    public  Gridadapter(Context applicationcontext, List<String> names){
+    public  Gridadapter(Context applicationcontext, List<String> names,List<Integer> images){
         this.names=names;
+        this.images=images;
         this.applicationcontext=applicationcontext;
         inflater=LayoutInflater.from(applicationcontext);
     }
@@ -40,6 +43,8 @@ public class Gridadapter extends BaseAdapter {
     public View getView(int i, View convertView, ViewGroup parent) {
         View v=inflater.inflate(R.layout.options,null);
         TextView oname=v.findViewById(R.id.tname);
+        ImageView imageView=v.findViewById(R.id.img);
+        imageView.setImageResource(images.get(i));
         oname.setText(names.get(i));
         return v;
     }
--- app/src/main/java/com/example/myquotes/MainActivity.java
+++ app/src/main/java/com/example/myquotes/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myquotes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,9 +14,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,25 +31,43 @@ public class MainActivity extends AppCompatActivity {
     Gridadapter gridadapter;
 
     List<String> name;
+    List<Integer> images;
+//    Integer[] images;
 
     Bundle a;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Intent intent=new Intent(MainActivity.this,PhoneSIgnIn.class);
-//        startActivity(intent);
         a=new Bundle();
         a=getIntent().getExtras();
         gridView=findViewById(R.id.grid);
+
+        images=new ArrayList<>();
+
+        images.add(R.drawable.nature);
+        images.add(R.drawable.mother);
+        images.add(R.drawable.father);
+        images.add(R.drawable.sister);
+        images.add(R.drawable.brother);
+        images.add(R.drawable.nation);
+        images.add(R.drawable.friends);
+        images.add(R.drawable.motivation);
+        images.add(R.drawable.heart);
+        images.add(R.drawable.facts);
+        images.add(R.drawable.fiction);
+        images.add(R.drawable.memories);
+        images.add(R.drawable.broken_heart);
+        images.add(R.drawable.news);
         name = new ArrayList<>();
         name.add("Nature");
         name.add("Mother");
         name.add("Father");
         name.add("Sister");
         name.add("Brother");
-        name.add("Country");
+        name.add("Nation");
         name.add("Friends");
         name.add("Motivation");
         name.add("Love");
@@ -52,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         name.add("Memories");
         name.add("Broken Heart");
         name.add("News");
-        gridadapter=new Gridadapter(this,name);
+        gridadapter=new Gridadapter(this,name,images);
         gridView.setAdapter(gridadapter);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -96,6 +120,31 @@ public class MainActivity extends AppCompatActivity {
             case  R.id.post:
                 Intent intent2=new Intent(MainActivity.this,Post.class);
                 startActivity(intent2);
+                return true;
+            case R.id.contact:
+                String userid=FirebaseAuth.getInstance().getCurrentUser().getUid();
+                ref=FirebaseDatabase.getInstance().getReference("Suggestions").child(userid).push();
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Enter Your Suggestion");
+                final EditText input = new EditText(MainActivity.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                alertDialog.setView(input);
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                alertDialog.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ref.child("Suggestions").setValue(input.getText().toString());
+                    }
+                });
+                    alertDialog.show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
